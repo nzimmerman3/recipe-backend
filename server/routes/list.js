f1fc1fe25b1e2ba8ee4771f496eb175b5eca99bb
nzimmerman3 <nicholaszimmerman1998@gmail.com>
2022-03-28 12:22:07 -0700

    Added delete functionality

--- server/routes/list.js
+++ server/routes/list.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const recordRoutes = express.Router();
 const dbo = require("../conn");
+var mongoose = require("mongoose");
 
 recordRoutes.route("/api").get(async function (_req, res) {
   const dbConnect = dbo.getDb();
@@ -20,7 +21,7 @@ recordRoutes.route("/api").get(async function (_req, res) {
 
 recordRoutes.route("/api").post((req, res) => {
   const dbConnect = dbo.getDb();
-  console.log("new message!");
+  console.log("new recipe:");
   console.log(req.body);
   dbConnect.collection("recipes").insertOne(req.body, (err, result) => {
     if (err) {
@@ -28,6 +29,19 @@ recordRoutes.route("/api").post((req, res) => {
     } else {
       console.log("Added new recipe");
       res.status(204).send();
+    }
+  });
+});
+
+recordRoutes.route("/api").delete((req, res) => {
+  const dbConnect = dbo.getDb();
+
+  const match = { _id: mongoose.Types.ObjectId(req.body.recipe._id) };
+  dbConnect.collection("recipes").deleteOne(match, (err, result) => {
+    if (err) {
+      req.status(400).send("Error deleting recipe");
+    } else {
+      console.log("Deleted recipe");
     }
   });
 });
